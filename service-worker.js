3770ab37663b0e82bfe8c0798edbcc0bc4ea807e
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-16 17:55:43 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = 'npat-pwa-v1';
+const staticAssets = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+    }));
+});
