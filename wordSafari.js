8f1defb3652efcd8e4c173012cb68961dce036dc
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-17 10:03:24 +0100

    Create wordSafari.js

---
+++ wordSafari.js
@@ -0,0 +1,14 @@
+const wordSafariData = {
+    week1: {
+        destination: "Africa",
+        categories: ["Animal from Africa", "Place in Africa", "African Food", "African Landmark"]
+    },
+    week2: {
+        destination: "Italy",
+        categories: ["Animal native to Italy", "City in Italy", "Italian Dish", "Italian Historical Site"]
+    },
+    // Add more weekly data here...
+};
+
+let currentWeek = 1;
+let passportSt
