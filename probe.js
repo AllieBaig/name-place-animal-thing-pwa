08292ded25000c8ddf931331566a3c55da4094c8
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-17 17:42:33 +0100

    Create probe.js

---
+++ probe.js
@@ -0,0 +1,14 @@
+/*!
+ * Name, Place, Animal, Thing PWA
+ * Copyright (c) 2025 AllieBaig
+ *
+ * This software is licensed under the MIT License.
+ * See LICENSE file for details: https://github.com/AllieBaig/name-place-animal-thing-pwa/blob/main/LICENSE
+ */
+
+// This file is intentionally left blank.
+// It can be used for debugging or probing purposes if needed.
+// For example, you could temporarily add logging statements here
+// to inspect variables or execution flow during development.
+
+console.log("probe.js loaded.");
